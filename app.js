3761a2ffffc10fbba9263c09d7e8156590db8470
musa al qadri <musaqazim1@gmail.com>
2023-10-11 12:09:32 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,58 @@
+// ------Chapter 2--------//
+
+// Question 1
+var myName = ("Musa al Qadri");
+alert(myName)
+ 
+// Question 2
+var age = ("15 years old")
+alert (age)
+
+// Question 3
+var certified = ("Certified Mobile Application Developnment");
+alert(certified)
+
+// Question 4
+var food = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+alert(food)
+
+// Question 5
+var email = ("My email adderss is example@example.com");
+alert(email)
+
+// Question 6
+var book = ("I am trying to learn from the Book A Smarter \nway to learn jawascript");
+alert(book)
+
+// Question 7
+var styla = ('“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”');
+alert(styla)
+
+// ------------chapter 3---------- // 
+
+// Question 1
+var nam = ("I am");
+var age = 15;
+var nama = ("years old");
+var myAge = nam+" "+age+" "+nama
+alert (myAge)
+
+// Question 2
+var visit = ("You have visited this site");
+var num = 14
+var tem = ("times");
+var full = visit + " " + num +" " +  tem
+alert(full)
+
+// Question 3
+var birth = "My birth year is"
+var year = 2001
+var happy = birth + " " +year
+alert (happy)
+
+// Question 4
+var name = "Musa Qazim ordered "
+var num = 5
+var nam = "T-shirt(s) on XYZ Clothing store"
+var marketing = name + " " +num+ " "+nam
+alert (marketing)
